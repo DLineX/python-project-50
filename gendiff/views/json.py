791940cab1aa9c23caa4fd726e50fe21b08e68d4
DaniilIslamgaliev <dante9226@mail.ru>
2024-07-14 16:49:55 +0300

    Add json.py

---
+++ gendiff/views/json.py
@@ -0,0 +1,14 @@
+import json
+
+
+from typing import Dict
+
+
+from gendiff.types import Node
+
+
+INDENT = 4
+
+
+def to_json(tree: Dict[str, Node]) -> str:
+    return json.dumps(tree, sort_keys=True, indent=INDENT, default=str)
