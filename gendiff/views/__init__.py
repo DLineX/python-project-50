e2e8f8ee27bd04db881101c16294e93bded62dcb
DaniilIslamgaliev <dante9226@mail.ru>
2024-07-14 16:50:48 +0300

    Update __init__.py

---
+++ gendiff/views/__init__.py
@@ -0,0 +1,14 @@
+from .stylish import to_stylish
+
+
+from .plain import to_plain
+
+
+from .json import to_json
+
+
+STYLISH = "stylish"
+PLAIN = "plain"
+JSON = "json"
+
+views = {STYLISH: to_stylish, PLAIN: to_plain, JSON: to_json}
