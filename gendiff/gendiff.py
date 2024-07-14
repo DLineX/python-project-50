0db7afb1ed1bdf8b9c37dc64c58635f289f520c1
DaniilIslamgaliev <dante9226@mail.ru>
2024-07-14 16:53:54 +0300

    Create gendiff main

---
+++ gendiff/gendiff.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+from gendiff.views import views, STYLISH
+
+
+from .io import load_content
+
+
+from .parsing import get_extension, parse
+
+
+from .tree import build_difference_tree
+
+
+def generate_diff(file1: Path, file2: Path, format: str = STYLISH) -> str:
+    file1_content, file2_content = load_content(file1), load_content(file2)
+    dict1 = parse(file1_content, get_extension(file1))
+    dict2 = parse(file2_content, get_extension(file2))
+    diff = build_difference_tree(dict1, dict2)
+    return views[format](diff)
