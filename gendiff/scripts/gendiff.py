aee075ec65f5d6ed79193328feca135ef0a0723c
DaniilIslamgaliev <dante9226@mail.ru>
2024-07-14 16:35:37 +0300

    Update gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,22 @@
 import argparse
 
 
+from gendiff import generate_diff
+
+
+from gendiff.views import STYLISH, PLAIN, JSON
+
+
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
     parser.add_argument(
-        '-f', '--format', metavar='FORMAT',
-        help='set format of output'
+        "-f", "--format", choices=[STYLISH, PLAIN, JSON], default=STYLISH,
+        metavar="FORMAT", help="set format of output"
     )
     args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
